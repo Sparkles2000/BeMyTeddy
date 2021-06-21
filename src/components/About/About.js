d7640a5574f1c29890636ad59ffd0bc0fbdfc74d
Anna <rucker.anna2000@gmail.com>
2021-06-20 21:34:08 -0500

    Making sure all updates are added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,12 +7,11 @@ function About() {
   return (
     <div className="about-container">
       <h1 className="about-heading">Be My Teddy Bear</h1>
-      {/* <img className="home-img" alt="Teddy" src={teddypic.jpg} /> */}
+
       <h2>Teddies need love too! Love a teddy! Care for a teddy! Adopt a teddy! Give a teddy a beary nice home! In return your teddy will take beary good care of you!</h2>
       <img className="about-img" alt="Teddy" src={teddypic} />
       <Link className="about-link" to="/Teddies">Show Me More!</Link>
     </div>
   )
 }
-
 export default About;
